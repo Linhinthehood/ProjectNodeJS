c6a38b2b4486799eeeef4c38befdce11beac3dcf
JackBao <522k0003@student.tdtu.edu.vn>
2025-03-27 16:08:27 +0700

    delete duplicate files

--- routes/orderRoutes 2.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-
-// POST /place-order
-router.post('/place-order', orderController.placeOrder);
-
-module.exports = router;
--- routes/profileRoutes 2.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.get('/profile', profileController.getProfile);
-
-module.exports = router;
